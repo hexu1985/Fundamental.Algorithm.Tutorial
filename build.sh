e6874fa5dc28c109559f21f0af19efc4997c9b45
mackhe <mackhe@tencent.com>
2021-04-21 11:51:06 +0800

    rename include -> src

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ current_dir=$(cd "$(dirname "$0")" && pwd)
 cmake -Bbuild -H. -DENABLE_UNIT_TESTS=ON -DGTest_DIR=${current_dir}/googletest/install/lib/cmake/GTest
 #-DGTest_DIR=必须写全路径，可以有'~'，但不能有'.'
 #cmake -G"MSYS Makefiles" -Bbuild -H. 
-cmake --build build/
-#cmake --build build/ -- -j 4
+#cmake --build build/
+cmake --build build/ -- -j 4
 #--target install
